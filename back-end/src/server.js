5c1b5874795675997fddf09c96365fd6b85d1998
Ashly Thomas Abraham <ashlythomas@gmail.com>
2023-07-10 21:56:42 +0400

    ready for deployment

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -12,6 +12,11 @@ app.use(express.json());
 
 app.use('/images', express.static(path.join(__dirname, '../assets')));
 
+app.use(express.static(
+    path.resolve(__dirname, '../dist'),
+    { maxAge: '1y', etag: false }
+));
+
 app.get('/api/hello', async(req, res) => {
     await client.connect();
     const db = client.db('vue-node-shopping');
@@ -86,6 +91,12 @@ app.delete('/api/users/:userId/cart/:productId', async(req, res) => {
     res.json(populatedCart);
 });
 
-app.listen(8000, () => {
-    console.log('Server is Listening on PORT: 8000');
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../dist/index.html'));
+});
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+    console.log('Server is Listening on PORT: ' + port);
 });
