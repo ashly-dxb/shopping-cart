da03a0338329bc3254b99bb86e23e868a5463e3d
Ashly Thomas Abraham <ashlythomas@gmail.com>
2024-06-17 12:39:54 +0400

    Loader in all pages

--- front-end/src/components/CartItems.vue
+++ front-end/src/components/CartItems.vue
@@ -1,21 +1,21 @@
 <template>
     <div v-for="product in items" class="product-container">
         <div class="details-wrap">
+            <img :src="require(`@/assets${product.imageUrl}`)" class="product-image" />
             <h3>{{ product.name }}</h3>
             <h3>AED {{ product.price }}</h3>
-            <img :src="require(`@/assets${product.imageUrl}`)" class="product-image" />
         </div>
 
         <div class="details-wrap">
-            <div class=" qty-container">
+            <div class="qty-container">
                 <div @click="$emit('decrease-qty', product.id)" class="qty-item qtyButtons">-</div>
-                <input type="text" :value="product.quantity" class="qty-item qtyDisplay" readonly />
+                <input type="text" :value="product.quantity" class="qty-item qtyInput" readonly />
                 <div @click="$emit('increase-qty', product.id)" class="qty-item qtyButtons">+</div>
             </div>
         </div>
 
         <div class="details-wrap">
-            <h3 class="text-right">Amount: AED {{ product.price * product.quantity }}</h3>
+            <h3 class="text-right">AED {{ product.price * product.quantity }}</h3>
         </div>
 
         <div class="remove-button" @click="$emit('remove-from-cart', product.id)">
